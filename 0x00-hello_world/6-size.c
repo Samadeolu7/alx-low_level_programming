1fde79bccb8d25963cf1c1ac0b0ff18ece7b9af1
Samuel Adeyemi <samadeolu7@gmail.com>
2022-06-30 15:57:30 -0700

    tria

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,17 @@
 /**
  *  main - Entry point
  *
- *  Return: Always 0 (success) 
+ *  Return: Always 0 (success)
  *
  */
 int main(void)
-{ 
+{
 	char a;
 	int b;
 	long int c;
 	long long int d;
 	float f;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
